55153b0e71f7bafb29575be16421764f743d74f9
yyoo@MP4 <ys7yoo@gmail.com>
2018-12-14 01:02:01 +0900

    input training dataset as argument

--- train_kNN_cv.py
+++ train_kNN_cv.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 from scipy.io import loadmat
 
@@ -20,30 +17,57 @@ from sklearn import metrics
 # http://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
 
 
-
-
 #from tqdm import tqdm # for progressive bar
 
 from sklearn.externals import joblib # to save model
 
-
 from P300.data import load_data
+
+
+import sys
+
+if len(sys.argv) < 2:
+    print ('choose training dataset (A, B, AandB)')
+    exit()
+
+subject_train = sys.argv[1]
+print(subject_train)
+
 
 
 # ## k-nearest neighbor classifier
 
-# In[2]:
-
 
 #subject_train = 'A'
-subject_train = 'B'
+#subject_train = 'B'
 #subject_test = 'A'
-subject_test = 'B'
-X_train, Y_train, Code_train = load_data(subject_train, 'train', 85)
-X_test, Y_test, Code_test = load_data(subject_test, 'test', 100)
+#subject_test = 'B'
+
+if subject_train is 'AandB':
+    subject = 'A'
+    X_train_A, Y_train_A, Code_train_A = load_data(subject, 'train', 85)
+    #X_test_A, Y_test_A, Code_test_A = load_data(subject, 'test', 100)
+
+    subject = 'B'
+    X_train_B, Y_train_B, Code_train_B = load_data(subject, 'train', 85)
+    #X_test_B, Y_test_B, Code_test_B = load_data(subject, 'test', 100)
+
+    # combine
+    X_train = np.vstack((X_train_A,X_train_B))
+    #print(X_train.shape)
+    Y_train = np.concatenate((Y_train_A, Y_train_B))
+    #print(Y_train.shape)
+
+    #X_test = np.vstack((X_test_A, X_test_B))
+    #print(X_test.shape)
+    #Y_test = np.concatenate((Y_test_A, Y_test_B))
+    #print(Y_test.shape)
+
+else:
+    X_train, Y_train, Code_train = load_data(subject_train, 'train', 85)
+    #X_test, Y_test, Code_test = load_data(subject_test, 'test', 100)
 
 
-# In[ ]:
 
 
 n_neighbors = np.arange(3, 10, 1, dtype=int)
@@ -55,4 +79,9 @@ clf = KNeighborsClassifier(n_jobs=-1)
 
 clf_cv = GridSearchCV(clf, parameters, cv=5)
 clf_cv.fit(X_train, Y_train)
+
+
+# save model
+subject = 'AandB'
+joblib.dump(clf_cv, "{}_kNN_cv.model".format(subject_train,))
 
